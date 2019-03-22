71bbe15a224c1400e56f6370e603163051acdf36
Blake Dietz <blakedietz@gmail.com>
2019-03-22 09:58:52 -0600

    docs(README.md): Include more detailed instructions (#3)
    * fix(Version): Remove version from output
    
    BREAKING CHANGE: Remove version from cli
    
    * docs(README.md): Update documenatation to show usage and install
    
    * chore: Add vscode debug and package-lock

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,19 +1,9 @@
 import * as cli from "commander";
-import * as fs from "fs";
-import * as path from "path";
 import { prettifyKeymapFile } from "../keymap";
-
-const { version } = JSON.parse(
-  fs
-    .readFileSync(
-      path.resolve(path.join(__dirname, "../../../../package.json"))
-    )
-    .toString()
-);
 
 export function runCLI(process: any) {
   cli
-    .version(version)
+    // .version(version)
     .option("-f --file <path>", "The file to prettify")
     .option(
       "-c --config <path>",
@@ -23,7 +13,6 @@ export function runCLI(process: any) {
 
   if (!Boolean(cli.file) && !Boolean(cli.config)) {
     throw new Error("Arguments config or file are not defined");
-    process.exit(1);
   }
 
   prettifyKeymapFile(cli.file, cli.config);
